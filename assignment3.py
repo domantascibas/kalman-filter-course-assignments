ecd65d081f9e9bec6e89530bcfab04f44f054282
Domantas Cibas <domantas@nectarsun.com>
2019-01-15 12:58:39 +0200

    first run
    first run with the 2D kalman filter implementation

--- assignment3.py
+++ assignment3.py
@@ -28,8 +28,8 @@ class KalmanFilter:
 
         # Next State Function
         self.F = np.matrix([[1, 0., 1, 0.],
-                            [0., 1, 0., 1]
-                            [0., 0., 1, 0.]
+                            [0., 1, 0., 1],
+                            [0., 0., 1, 0.],
                             [0., 0., 0., 1]])
 
         # Measurement Function
@@ -46,11 +46,30 @@ class KalmanFilter:
                             [0., 0., 1., 0.],
                             [0., 0., 0., 1.]])
     def predict(self, dt):
-        return
+      # dt is given, so need to plug it into F and calculate x
+      self.F[0,2] = dt
+      self.F[1,3] = dt
+
+      self.x = self.F * self.x
+      self.P = self.F * self.P * np.transpose(self.F)
+      return
+    
     def measure_and_update(self,measurements, dt):
-        return [self.x[0], self.x[1]]
+      # again, update the F matrix with dt:
+      self.F[0,2] = dt
+      self.F[1,3] = dt
+
+      Z = np.matrix(measurements)
+      y = np.transpose(Z) - self.H * self.x
+      S = self.H * self.P * np.transpose(self.H) + self.R
+      K = self.P * np.transpose(self.H) * np.linalg.inv(S)
+
+      self.x = self.x + K * y
+      self.P = (self.I - K * self.H) * self.P
+
+      return [self.x[0], self.x[1]]
 
     def recieve_inputs(self, u_steer, u_pedal):
-        return
+      return
 
 sim_run(options,KalmanFilter)
